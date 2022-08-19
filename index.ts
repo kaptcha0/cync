11bca55379c939a6c3f7dd658cca831aeffe955e
Kaptcha <50426223+re-kaptcha@users.noreply.github.com>
2022-08-19 19:58:39 +0000

    Fixed cron job formatting, added (crude)logging, and moved actual code to index.ts

--- index.ts
+++ index.ts
@@ -1,23 +1,181 @@
-import http from 'http'
-import { job, setup, status } from './cync'
+import nodeCron from 'node-cron'
+import { Client } from "@notionhq/client";
+import { markdownToBlocks } from '@tryfabric/martian';
+import fetch from 'cross-fetch'
+import ICalParser from 'ical-js-parser';
 
-// Needed for keep-alive functionality on free repl.
-const server = http.createServer((_, res) => {
-	res.writeHead(status.code)
-	res.end(status.message)
+const notion = new Client({
+	auth: process.env.NOTION_KEY
 })
 
-server.listen(0, '0.0.0.0', async () => {
-	const addr = server.address()
+const calendarId = process.env.NOTION_CALENDAR_ID
+const settingsId = process.env.NOTION_SETTINGS_ID
 
+const calendars: {
+	[key: string]: {
+		name: string,
+		url: string
+	}
+} = {}
 
-	console.log("[SETUP] Setting up settings databases in Notion...")
+function parseDesc(desc: string) {
+	let res = unescape(desc)
+	res = res.replaceAll('\\n', '\n')
+
+	return res
+}
+
+export async function sync() {
+	if (!calendarId)
+		throw new Error("NOTION_CALENDAR_ID is not defined in the environment variables")
+
+	console.log("[sync] Synchronizing...")
+
+	for (let id in calendars) {
+		const { name, url } = calendars[id]
+
+		// Pull iCal from Google Calendar
+		const res = await fetch(url)
+		const ical = await res.text()
+
+		// Parse iCal
+		const json = ICalParser.toJSON(ical)
+
+		console.log('[sync] Calendar "%s"', name)
+		for (let [i, ev] of json.events.entries()) {
+			console.log(`[sync] Doing ${i + 1}/${json.events.length}`)
+			let start = ev.dtstart.value
+			let tz = ev.dtstart.timezone
+			let end = ev.dtend ? ev.dtend.value : null
+			let { summary, description, uid, url } = ev
+
+			const exists = async (id: string | undefined) => {
+				const res = await notion.databases.query({
+					database_id: calendarId,
+					filter: {
+						property: "ID",
+						rich_text: {
+							equals: id as string
+						}
+					}
+				})
+
+				return res.results.length > 0
+			}
+
+			// Skip if it already exists
+			if (await exists(uid)) {
+				continue
+			}
+
+			const rt = description ? parseDesc(description) : ""
+
+			const children = markdownToBlocks(rt)
+
+			const properties = {
+				Event: {
+					title: [
+						{
+							type: "text",
+							text: {
+								content: summary as string
+							}
+						}
+					]
+				},
+				Calendar: {
+					type: "select",
+					select: { name }
+				},
+				Date: {
+					type: "date",
+					date: {
+						start,
+						end,
+						time_zone: tz
+					}
+				},
+				ID: {
+					rich_text: [
+						{
+							type: "text",
+							text: {
+								content: ev.uid
+							}
+						}
+					]
+				},
+
+			}
+
+			if (url) {
+				properties.URL = {
+					type: "url",
+					url: url
+				}
+			}
+
+			await notion.pages.create({
+				children,
+				parent: {
+					type: "database_id",
+					database_id: calendarId
+				},
+				properties
+			})
+		}
+		console.log("")
+	}
+
+	console.log("[sync] Done.")
+}
+
+export async function setup() {
+	if (!settingsId)
+		throw new Error("NOTION_SETTINGS_ID is not defined in the environment variables")
+
+	const res = await notion.databases.query({
+		database_id: settingsId
+	})
+
+	for (let result of res.results) {
+		let urlProp = result.properties["URL"].id
+		let nameProp = result.properties["Name"].id
+
+		const name = (
+			await notion.pages.properties.retrieve({
+				page_id: result.id,
+				property_id: nameProp
+			})
+		).results[0].title.plain_text
+
+		const url = (
+			await notion.pages.properties.retrieve({
+				page_id: result.id,
+				property_id: urlProp
+			})
+		).url
+
+		calendars[result.id] = { name, url }
+	}
+
+	await sync()
+}
+
+// Run every 5 minutes
+export const job = nodeCron.schedule("0 5 * * * *", async () => {
+	const t = new Date().toISOString
+
+	status.message = `[${t()}] Synchronizing`
+	console.log("\n[CRON][%s] Synchronizing Google Calendar and Notion...", t())
 	await setup()
-	console.log("[SETUP] Notion settings database setup\n")
-
-	console.log("[SETUP] Starting cron job")
-	job.start()
-	console.log('[SETUP] Cron job started successfully\n')
-
-	console.log(`[SERVER] Listening on ${addr.address}:${addr.port}`)
+	await sync()
+	console.log("[CRON][%s] Finished.\n", t())
+	status.message = `[${t()}] Synchronized`
 })
+
+// Status info to be displayed on server
+export const status = {
+	code: 200,
+	message: 'OK'
+}
